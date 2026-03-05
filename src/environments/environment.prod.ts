a6ac637d4a7521015720f0ad14163af99b971374
Shacky1 <meshackvenance99@gmail.com>
2026-03-05 22:16:27 +0300

    changes made

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
-
 export const environment = {
   production: true,
   tmdbApiKey: 'undefined',
   tmdbBaseUrl: 'https://api.themoviedb.org/3',
-  tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500'
+  tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500',
 };
